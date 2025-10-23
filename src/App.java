0d1559b48138375989e940f7eddecd5f1043ea85
ingJuanArenas <juanarenasing@gmail.com>
2025-10-23 12:21:32 -0500

    working

--- src/App.java
+++ src/App.java
@@ -1,5 +1,60 @@
+import java.util.Scanner;
+
+import Model.Car;
+import Service.CarService;
+
 public class App {
+
+    private static Scanner sc= new Scanner(System.in);
+    private static CarService carService= new CarService();
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean salir = false;
+
+        do {
+                    System.out.println("""
+            ==== LOTE====
+            1. ver carros
+            2. agregar carro
+            3. consultar carro
+            4. borrar carro
+            0.salir
+                """);
+
+        var op= sc.nextInt();
+
+        switch (op) {
+            case 1:
+                carService.getCars();
+                break;
+            case 2:
+                System.out.println("Model");
+                var  model = sc.nextLine();
+                System.out.println("Year;");
+                var year = sc.nextInt();
+                System.out.println("serial");
+                var serial0 = sc.nextLine();
+                System.out.println("Price");
+                var price = sc.nextDouble();
+                carService.addCar(new Car(model, year, serial0, price));
+                break;
+            case 3:
+            sc.nextLine();
+            System.out.println("INgrese el serial");
+            var serial = sc.nextLine();
+                System.err.println(carService.getCarBySerial(serial));
+                break;
+            case 4:
+            sc.nextLine();
+            System.out.println("INgrese el serial");
+            var serial1 = sc.nextLine();
+            carService.deleteCar(serial1);
+            break;
+            case 0:
+            default:
+            System.out.println("Opcion invalida");
+                break;
+        }
+        } while (salir != true);
+
     }
 }
