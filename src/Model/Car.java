4b064d9529e9d74a785173f31bf909ba695c5efc
ingJuanArenas <juanarenasing@gmail.com>
2025-10-23 12:20:03 -0500

    car class

---
+++ src/Model/Car.java
@@ -0,0 +1,48 @@
+package Model;
+
+public class Car {
+    private String model;
+    private int year;
+    private String serial;
+    private double price;
+
+    
+    public Car(String model, int year, String serial, double price) {
+        this.model = model;
+        this.year = year;
+        this.serial = serial;
+        this.price = price;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public String getSerial() {
+        return serial;
+    }
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    @Override
+    public String toString() {
+        return "Car [model: " + model + ", year: " + year + ", serial: " + serial + ", price: " + price + "]";
+    }
+
+
+    
+    
+}
